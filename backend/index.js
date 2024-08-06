6c88b9ed89f816547bc57d35715c1b887527f7d8
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-08-06 10:19:11 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,18 @@ const app = express();
 const TopHeadLinesRoute = require("./routes/TopHeadlinesRoute.js");
 const EverthingInNewsRoute = require("./routes/EverthingInNewsRoute.js");
 
-const API_KEY = process.env.API_KEY;
-
 dotenv.config({
 	path: "./.env",
 });
 
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT;
+
 app.get("/", (req, res) => {
 	res.json("Hello");
 });
+
 app.use("/topheadlines", TopHeadLinesRoute);
 app.use("/everything", EverthingInNewsRoute);
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app; // Export the app
